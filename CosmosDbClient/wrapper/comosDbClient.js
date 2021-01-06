164760c038f1835d18c5930a82a4223b6dfb89ea
AimalKhanOfficial <aimalkhanofficial@gmail.com>
2021-01-06 16:02:48 -0500

    Removing the extra comment

--- CosmosDbClient/wrapper/comosDbClient.js
+++ CosmosDbClient/wrapper/comosDbClient.js
@@ -32,7 +32,6 @@ const saveToCosmosDb = async (name, employeeId, address) => {
 const isObject = (empNameParam) => typeof empNameParam === 'object';
 const convertToString = (empName) => isObject(empName) ? JSON.stringify(empName) : empName.toString();
 
-//Extra comment - not needed
 const retrieveEmployeeData = async (empName) => {
     try {
         console.log('>> findOne query name:', isObject(empName), convertToString(empName), typeof convertToString(empName));
